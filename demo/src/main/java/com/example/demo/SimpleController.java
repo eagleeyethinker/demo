2ec62f18d5241af52d7257538a6b19cc76f5b72a
Satish <eagleeyethinker@gmail.com>
2020-12-26 13:34:09 -0500

    Final

--- demo/src/main/java/com/example/demo/SimpleController.java
+++ demo/src/main/java/com/example/demo/SimpleController.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
     @GetMapping("/hello")
-    public  String hello(@RequestParam String name)
+    public @ResponseBody String hello(@RequestParam String name)
     {
         return  String.format("Hello %s",name);
     }
